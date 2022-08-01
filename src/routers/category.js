a7abd000210e3f519a319e63e5fd4bd3e69c0bbb
LaTienToan <101979826+LaTienToan@users.noreply.github.com>
2022-08-01 15:33:53 +0700

    Create api remove Category

--- src/routers/category.js
+++ src/routers/category.js
@@ -5,7 +5,7 @@ import { remove_category } from "../apis/categories/remove_category"
 const router = express.Router()
 
 router.post("/category", add_category)
-router.delete("/category:id", remove_category)
+router.delete("/category/:id", remove_category)
 
 
 export default router
