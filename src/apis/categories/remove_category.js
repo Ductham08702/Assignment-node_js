26fcfc83d91c9f39641f9191090f9482f4847eb2
LaTienToan <101979826+LaTienToan@users.noreply.github.com>
2022-08-01 15:24:47 +0700

    create_api_add_category

---
+++ src/apis/categories/remove_category.js
@@ -0,0 +1,13 @@
+// import Category from "../../models/categories";
+
+// export const remove_category = async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         const category = await Category.findOneAndDelete({ _id: id }).exec()
+//         res.json(category)
+//     } catch (error) {
+//         res.status(400).json({
+//             error: "khong xoa duoc danh muc"
+//         })
+//     }
+// }
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import express from "express"
 import cors from "cors"
 import "dotenv/config";
-// import routerProduct from "./routers/product";
+import routerProduct from "./routers/product";
 import routerCategories from "../src/routers/category"
 
 
@@ -12,7 +12,7 @@ const port = 8001;
 
 app.use(cors());
 app.use(express.json())
-// app.use("/api", routerProduct)
+app.use("/api", routerProduct)
 app.use("/api", routerCategories)
 
 
