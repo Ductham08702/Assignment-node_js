ec542d260fad6bd45e1fc34d1519aca993395066
LaTienToan <101979826+LaTienToan@users.noreply.github.com>
2022-08-01 15:41:08 +0700

    Create api list Category

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { add_category } from "../apis/categories/add_categories"
+import { list_category } from "../apis/categories/list_category"
 import { remove_category } from "../apis/categories/remove_category"
 
 const router = express.Router()
 
 router.post("/category", add_category)
 router.delete("/category/:id", remove_category)
+router.get("/category/", list_category)
 
 
 export default router
